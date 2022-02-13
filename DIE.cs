05bc5e1c9bfb0c1dd0071bfcad3c84b6ac55f9ee
Ahjay803 <93839913+Ahjay803@users.noreply.github.com>
2022-02-13 06:41:13 -0800

    Add files via upload

---
+++ DIE.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Allen Atkins
+ * CST-150 
+ * THIS IS MY WORK 
+ */
+namespace DICEGAME_ACTIVITY_11_
+{
+   public class DIE
+    {
+        private int NUMBER_OF_SIDES;
+        private static Random FACE_SIDE = new Random();
+
+        public DIE(int NUMBER_OF_SIDES)
+        {
+            if (NUMBER_OF_SIDES > 4 || NUMBER_OF_SIDES > 20)
+            {
+                Console.WriteLine("Number should be in the range between 4 and 20.");
+                
+            }
+            this.NUMBER_OF_SIDES = NUMBER_OF_SIDES;
+        }
+
+        public int rollDie()
+        {
+
+            return FACE_SIDE.Next(1, NUMBER_OF_SIDES + 1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Allen Atkins 
+ * cst-150 
+ * THIS IS MY WORK!!
+ */
+namespace DICEGAME_ACTIVITY_11_
+{
+    class Program
+    {
+        public static void Main()
+        {
+            DIE die1 = new DIE(6);
+            DIE die2 = new DIE(6);
+
+            int die1roll, die2roll;
+            int rolls = 0;
+
+
+
+            do
+            {
+                die1roll = die1.rollDie();
+                die2roll = die2.rollDie();
+                Console.WriteLine("Rolled [ " + "die1: " + die1roll + " die2: " + die2roll + " ]");
+                rolls++;
+
+            }
+            while (die1roll != 1 || die2roll != 1);
+            Console.WriteLine("\n It took user " + rolls + " rolls to get snake eyes.");
+            Console.ReadLine();
+        }
+    }
+}
